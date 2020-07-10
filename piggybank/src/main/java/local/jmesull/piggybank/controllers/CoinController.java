39fbab670d0a14a5c93eed88c6d788b6a8938f7e
Jamie Sullivan <jamiesullivan@jamies-mbp.lan>
2020-07-10 13:57:53 -0400

    some of stretch complete

--- piggybank/src/main/java/local/jmesull/piggybank/controllers/CoinController.java
+++ piggybank/src/main/java/local/jmesull/piggybank/controllers/CoinController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -51,4 +52,60 @@ public class CoinController
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
+    //http://localhost:2019/money/{amount}
+
+    @GetMapping(value = "/money/{amount}",
+    produces = {"application/json"})
+    public ResponseEntity<?> removeCoins(@PathVariable double amount)
+    {
+        List<Coin> myList = new ArrayList<>();
+        coinrepo.findAll().iterator().forEachRemaining(myList::add);
+
+        double total = 0;
+
+        for(Coin c : myList)
+        {
+            total = total + (c.getQuantity() * c.getValue());
+        }
+
+
+        if(amount > total)
+        {
+            System.out.println("You don't have enough in the piggy bank!");
+        }
+
+
+        for(Coin c: myList)
+        {
+                if (c.getQuantity() > 1 || c.getQuantity() == 0)
+                {
+                    if (c.getNamepural()
+                        .contains("ollar"))
+                    {
+                        System.out.println("$" + c.getQuantity());
+                    } else
+                    {
+                        System.out.println(c.getQuantity() + " " + c.getNamepural());
+                    }
+                }
+
+                if (c.getQuantity() == 1)
+                {
+                    if(c.getName().contains("ollar"))
+                    {
+                        System.out.println("$" + c.getQuantity());
+                    }
+                    else
+                    {
+                    System.out.println(c.getQuantity() + " " + c.getName());
+                    }
+            }}
+
+        System.out.println("The piggy bank holds " + total);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
 }
