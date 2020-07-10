26f760ef824dd2097a174f4ad9dc56d9015095d9
Jamie Sullivan <jamiesullivan@jamies-mbp.lan>
2020-07-10 11:22:05 -0400

    created project

---
+++ piggybank/src/main/java/local/jmesull/piggybank/PiggybankApplication.java
@@ -0,0 +1,16 @@
+package local.jmesull.piggybank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiggybankApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(PiggybankApplication.class,
+            args);
+    }
+
+}
