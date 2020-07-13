8961ea981f2d9b82707c6609a7d2644899f508ad
Jamie Sullivan <jamiesullivan@jamies-mbp.lan>
2020-07-13 16:27:19 -0400

    conditionals are kinda there

--- piggybank/src/main/java/local/jmesull/piggybank/controllers/CoinController.java
+++ piggybank/src/main/java/local/jmesull/piggybank/controllers/CoinController.java
@@ -62,18 +62,120 @@ public class CoinController
         List<Coin> myList = new ArrayList<>();
         coinrepo.findAll().iterator().forEachRemaining(myList::add);
 
-        double total = 0;
-
+        double initalTotal = 0;
         for(Coin c : myList)
         {
-            total = total + (c.getQuantity() * c.getValue());
+            initalTotal = initalTotal + (c.getQuantity() * c.getValue());
         }
 
-
-        if(amount > total)
+        if(amount > initalTotal)
         {
             System.out.println("You don't have enough in the piggy bank!");
         }
+
+        if(amount <= initalTotal)
+        {
+                int dollars = (int) amount;
+                double cents = (amount - dollars) * 100;
+                int intCents = (int) cents;
+
+                int dimes = intCents / 10;
+                intCents = intCents - (dimes * 10);
+                int nickles = intCents / 5;
+                intCents = intCents - (nickles * 5);
+                int pennies = intCents / 1;
+
+                //subtract the amount
+                for(Coin c : myList)
+                {
+                    if (c.getName().contains("ollar"))
+                    {
+                        int initalQuantity = c.getQuantity();
+
+                        if (initalQuantity >= dollars)
+                        {
+                            c.setQuantity(c.getQuantity() - dollars);
+                            dollars = 0;
+                        }
+
+                        if (initalQuantity < dollars)
+                        {
+                            c.setQuantity(0);
+                            dollars = dollars - initalQuantity;
+                        }
+
+                    }
+
+                    if (c.getName().contains("arter"))
+                    {
+                        int initalQuantity = c.getQuantity();
+                        int quarters = intCents / 25;
+                        intCents = intCents - (quarters * 25);
+                        if (initalQuantity >= quarters)
+                        {
+                            c.setQuantity(c.getQuantity() - quarters);
+                            quarters = 0;
+                        }
+
+                        if (initalQuantity < quarters)
+                        {
+                            c.setQuantity(0);
+                            quarters = quarters - initalQuantity;
+                        }
+
+                    }
+
+                    if (c.getName().contains("ime"))
+                    {
+                        int initalQuantity = c.getQuantity();
+                        if (initalQuantity >= dimes)
+                        {
+                            c.setQuantity(c.getQuantity() - dimes);
+                            dimes = 0;
+                        }
+
+                        if (initalQuantity < dimes)
+                        {
+                            c.setQuantity(0);
+                            dimes = dimes - initalQuantity;
+                        }
+
+                    }
+
+                    if (c.getName().contains("ickle"))
+                    {
+                        int initalQuantity = c.getQuantity();
+                        if (initalQuantity >= nickles)
+                        {
+                            c.setQuantity(c.getQuantity() - nickles);
+                            nickles = 0;
+                        }
+
+                        if (initalQuantity < nickles)
+                        {
+                            c.setQuantity(0);
+                            nickles = nickles - initalQuantity;
+                        }
+                    }
+
+                    if (c.getName().contains("ennie"))
+                    {
+                        int initalQuantity = c.getQuantity();
+
+                        if (initalQuantity >= pennies)
+                        {
+                            c.setQuantity(c.getQuantity() - pennies);
+                            pennies = 0;
+                        }
+
+                        if (initalQuantity < pennies)
+                        {
+                            c.setQuantity(0);
+                            pennies = pennies - initalQuantity;
+                        }
+
+                }
+        }}
 
 
         for(Coin c: myList)
@@ -101,6 +203,12 @@ public class CoinController
                     System.out.println(c.getQuantity() + " " + c.getName());
                     }
             }}
+
+        double total = 0;
+        for(Coin c : myList)
+        {
+            total = total + (c.getQuantity() * c.getValue());
+        }
 
         System.out.println("The piggy bank holds " + total);
 
